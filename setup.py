03ad9c2ef9a3cf2dca4fb3347321dd516bd05eaf
alexweb01 <alexweb01@gmail.com>
2024-09-21 22:03:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import types
 from importlib import load_source
 from os import path
 from sys import argv
@@ -36,6 +37,36 @@ MODULES = (
     "archive", "audio", "container", "common", "file_system", "game",
     "image", "misc", "network", "program", "video")
 
+def import_source_file(fname: str | Path, modname: str) -> "types.ModuleType":
+     """
+     Import a Python source file and return the loaded module.
+
+     Args:
+         fname: The full path to the source file.  It may container characters like `.`
+             or `-`.
+         modname: The name for the loaded module.  It may contain `.` and even characters
+             that would normally not be allowed (e.g., `-`).
+     Return:
+         The imported module
+
+     Raises:
+         ImportError: If the file cannot be imported (e.g, if it's not a `.py` file or if
+             it does not exist).
+         Exception: Any exception that is raised while executing the module (e.g.,
+             :exc:`SyntaxError).  These are errors made by the author of the module!
+     """
+     # https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
+     spec = importlib.util.spec_from_file_location(modname, fname)
+     if spec is None:
+         raise ImportError(f"Could not load spec for module '{modname}' at: {fname}")
+     module = importlib.util.module_from_spec(spec)
+     sys.modules[modname] = module
+     try:
+         spec.loader.exec_module(module)
+     except FileNotFoundError as e:
+         raise ImportError(f"{e.strerror}: {fname}") from e
+     return module
+    
 def main():
     if "--setuptools" in argv:
         argv.remove("--setuptools")
@@ -46,7 +77,7 @@ def main():
         use_setuptools = False
 
 
-    hachoir_parser = load_source("version", path.join("hachoir_parser", "version.py"))
+    hachoir_parser = import_source_file("version", path.join("hachoir_parser", "version.py"))
     PACKAGES = {"hachoir_parser": "hachoir_parser"}
     for name in MODULES:
         PACKAGES["hachoir_parser." + name] = "hachoir_parser/" + name
